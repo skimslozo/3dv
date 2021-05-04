de1f3e44984d9ae700a0ce8ea6ecabf3f5115c6d
Jason <corkillj@student.ethz.ch>
2021-05-04 13:14:10 -0700

    added steps

--- code/Triangulation.py
+++ code/Triangulation.py
@@ -10,16 +10,19 @@ sys.path.append(str(Path(__file__).absolute().parent.parent / 'football'))
 
 
 datamanager = DataManager()
-data = datamanager.load_data('run1_data.p')
-constants = datamanager.load_constants('run1_constants.p')
+datamanager2 = DataManager()
+data = datamanager.load_data('test_run', 'test_run_data.p')
+constants = datamanager.load_constants('test_run', 'test_run_constants.p')
+data2 = datamanager2.load_data('test_run2', 'test_run2_data.p')
+constants2 = datamanager2.load_constants('test_run2', 'test_run2_constants.p')
 
 k_0 = np.array(constants['intrinsic_mat'])
 points_2d_0 = datamanager.get_points_2d(0)
-points_2d_1 = datamanager.get_points_2d(1)
+points_2d_1 = datamanager.get_points_2d(2)
 ext_mat_0 = datamanager.get_ext_mat(0)
-ext_mat_1 = datamanager.get_ext_mat(1)
+ext_mat_1 = datamanager.get_ext_mat(2)
 gt_points_3d = datamanager.get_points_3d()
-
+gt_points_3d2 = datamanager2.get_points_3d()
 proj_mat_0 = k_0 @ ext_mat_0
 proj_mat_1 = k_0 @ ext_mat_1
 
--- football/datagen.py
+++ football/datagen.py
@@ -1,5 +1,5 @@
 import numpy as np
-import generate_dataset as gd
+import DatasetGenerator
 
 camera_positions = np.array([[-40, -140, 70],
                              [40, -140, 70],
@@ -7,5 +7,6 @@ camera_positions = np.array([[-40, -140, 70],
 camera_rotations = np.array([[60, 0, 0],
                              [60, 0, 0],
                              [60, 0, 0]])
-gd.generate_dataset('test_run', camera_positions, camera_rotations)
+dg = DatasetGenerator.DatasetGenerator()
+dg.generate_dataset('test_run2', camera_positions, camera_rotations)
 
