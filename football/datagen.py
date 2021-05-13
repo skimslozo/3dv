6746d5b10b164e1d7d63eaf1160674510b6d8802
Jannik <brundli@yahoo.de>
2021-05-13 13:03:00 +0200

    replay

--- football/datagen.py
+++ football/datagen.py
@@ -3,25 +3,33 @@ import DatasetGenerator
 import DataManager
 
 
+
+
+
 camera_positions = np.array([[ 0, -80, 60],
                              [-60, -140, 70],
                              [60, -140, 70],
                              [0, -140, 70],
                              [-60, 140, 70],
                              [60, 140, 70],
-                             [0, 140, 70]])
+                             [0, 140, 70,],
+                             [0, 0, 80],])
 camera_rotations = np.array([[50,0, 0],
                              [60, 0, -20],
                              [60, 0, 20],
                              [60, 0, 0],
                              [60, 0, -160],
                              [60, 0, 160],
-                             [60, 0, 180]])
+                             [60, 0, 180],
+                             [0, 0, 180],
+                             ])
 dg = DatasetGenerator.DatasetGenerator()
-dg.generate_dataset('test', camera_positions, camera_rotations, steps=200, save_frames=True, write_video=True, use_red_dot=True)
-
-
+dg.generate_dataset('othergroup', camera_positions, camera_rotations, steps=200, save_frames=True, write_video=True, use_red_dot=False)
 
 a = DataManager.DataManager()
-a.write_csv_proj('valid')
+a.write_csv_proj('othergroup')
+
+
+
+
 
