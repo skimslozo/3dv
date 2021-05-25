50a434ecc466a465bd1e0f2811927fc817f1a77f
Jason <corkillj@student.ethz.ch>
2021-05-25 14:51:19 -0700

    add assertion

--- football/datagen.py
+++ football/datagen.py
@@ -5,7 +5,6 @@ import DataManager
 camera_positions = np.loadtxt('camera_positions.csv', delimiter=",", skiprows=1)
 camera_rotations = np.loadtxt('camera_rotations.csv', delimiter=",", skiprows=1)
 assert camera_positions.shape[0] == camera_rotations.shape[0], 'Not the same amount of entries in \'camera_positions.csv\' as in \'camera_rotations.csv\''
-
 dg = DatasetGenerator.DatasetGenerator()
 dg.generate_dataset('test_run.csv',
                     camera_positions,
