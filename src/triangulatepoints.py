f620980938fd185743d1e08718395fa251835150
Jason <corkillj@student.ethz.ch>
2021-05-27 03:18:05 -0700

    adds ability to deal with out of bounds flags

--- src/triangulatepoints.py
+++ src/triangulatepoints.py
@@ -44,7 +44,7 @@ def tri_residual(point_3d, **kwargs):
     return residual
 
 
-def triangulate_points_nonlinear_refinement(proj_mat_all, points_2d_all):
+def triangulate_points_nonlinear_refinement(proj_mat_all, points_2d_all, oob_flags_all, method='lm'):
     """
     Triangulates the real world position of a point in world coordinates in N time steps based on M views \
     using the Nonlinear Refinement technique.
@@ -52,20 +52,42 @@ def triangulate_points_nonlinear_refinement(proj_mat_all, points_2d_all):
     @param proj_mat_all: M X N X 3 X 4 array, containing projection Matrices where M is the number of cameras \
     and N is the amount of steps.
     @param points_2d_all: M X N X 2 array, containing the position of the ball in pixel coordinates for each \
-    frame n and each camera M
+    frame n and each camera m.
+    @param oob_flags_all: M X N array, containing the out of bounds flags for each frame n and each camera m.
+    @param method: method to be used by the least_squares function, default is 'lm', but this method only works for 3
+    or more cameras, if there are only two cameras the method ‘trf’ should be used.
     @return: 3d_points, triangulated 3d position of the ball
     """
     shape = proj_mat_all.shape
     amount_cam = shape[0]
     steps = shape[1]
 
+    if amount_cam == 2 and method == 'lm':
+        print('Warning: Only two camera views where given but method was set to \'lm\' which needs at least three'
+              ' views. Method was changed to \'trf\'.')
+        method = 'trf'
+    assert amount_cam >= 2, 'Not enough cameras to triangulate, only {} cameras'.format(amount_cam)
+
     initial_guess_3d = np.zeros((3, 1)).flatten()
     points_3d = np.zeros((steps, 3))
     for step in range(steps):
-        cur_proj_mat_all = proj_mat_all[:, step, :, :]
-        cur_points_2d_all = points_2d_all[:, step, :]
-        ls = least_squares(tri_residual, initial_guess_3d, method='lm', kwargs={'proj_mat_all': cur_proj_mat_all,
+        cur_oob_flags = oob_flags_all[:, step]
+        cur_proj_mat_all = proj_mat_all[~cur_oob_flags, step, :, :]
+        cur_points_2d_all = points_2d_all[~cur_oob_flags, step, :]
+
+        cur_amount_cam = cur_points_2d_all.shape[0]
+        if cur_amount_cam == 2 and method == 'lm':
+            print('Warning: Only two camera views where given but method was set to \'lm\' which needs at least three'
+                  ' views. Method was changed to \'trf\'.')
+            cur_method = 'trf'
+        else:
+            cur_method = method
+        assert cur_amount_cam >= 2, 'Not enough cameras to triangulate, only {amount} cameras in step {cur_step}'.format(
+            amount=cur_amount_cam, cur_step=step)
+
+        ls = least_squares(tri_residual, initial_guess_3d, method=cur_method, max_nfev=10000, kwargs={'proj_mat_all': cur_proj_mat_all,
                                                                                 'point_2d_all': cur_points_2d_all,
-                                                                                'amount_cam': amount_cam})
+                                                                                'amount_cam': cur_amount_cam})
         points_3d[step] = ls.x
+
     return points_3d
