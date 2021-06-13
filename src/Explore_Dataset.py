4575f1859935bb9991c64ea23b7d1d6539443689
Jason <corkillj@student.ethz.ch>
2021-06-13 12:22:45 -0700

    adds comments

--- src/Explore_Dataset.py
+++ src/Explore_Dataset.py
@@ -1,5 +1,6 @@
 """
-Script to run the various triangulations and vispy visualizations
+Script to run the various triangulations, error metrics and vispy visualizations. Use as an example of how to work with
+this pipeline.
 """
 
 import vispy
@@ -16,9 +17,11 @@ from triangulatepoints import triangulate_points_two_views as tri_tv
 from triangulatepoints import triangulate_points_nonlinear_refinement as tri_nlr
 from src.TrajectoryEstimation import interpolate_3d
 
+'''DataManager Objects allows you to easily load datasets you've generated with the dataset generator, it can also
+manipulate them to eg. add noise to 2d pixel coordinates'''
 datamanager = DataManager()
 
-data, constants = datamanager.load('test_run2')
+data, constants = datamanager.load('test_run2')  # change name here to the desired dataset
 proj_mat_all = datamanager.get_proj_mat_all()
 points_2d_all = datamanager.get_points_2d_all(set_oob_nan=True)
 points_2d_noise_all = datamanager.get_points_2d_noise_all(set_oob_nan=True, std=10)
@@ -35,6 +38,7 @@ points_3d_noise_tv = tri_tv(proj_mat_all[1], proj_mat_all[2], points_2d_noise_al
 
 points_gt_fine = interpolate_3d(np.transpose(gt_points_3d), gt_points_3d.shape[0]*100)
 
+'''Example use of the EstimationMetrics class'''
 estimator_tv = EstimationMetrics(gt_points_3d, points_3d_tv)
 estimator_nlr = EstimationMetrics(gt_points_3d, points_3d_nlr)
 estimator_nlr_noise = EstimationMetrics(gt_points_3d, points_3d_noise_nlr)
@@ -51,6 +55,7 @@ print('The largest error for two views with noise is: \n', estimator_tv_noise.la
 print('The largest error for {} views using Nonlinear Refinement with noisy data is: \n'.format(amount_cam),
       estimator_nlr_noise.largest_difference())
 
+'''This is how you can use the Visualizer to Visualize the 3D ball coordinates'''
 visualizer = SoccerVisualizer()
 visualizer.draw_ball_marker(points_3d_tv, size_marker=7, color='red')
 visualizer.draw_ball_marker(points_3d_nlr, size_marker=7, color='blue')
